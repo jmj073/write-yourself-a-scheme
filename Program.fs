d69359b30fb2a808cc520b1b49155cf31e409d7b
jmj073 <wjsalswns0733@gmail.com>
2025-02-16 18:51:59 +0900

    sln 최신것으로

--- Program.fs
+++ Program.fs
@@ -1,14 +1,2 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open NUnit.Framework
-open FsUnit
-
-[<Test>]
-let ``test framework`` () =
-    1 |> should equal 1
-
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
